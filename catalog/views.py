fa369aa265ee56e28d38ee5d919f5234c76d15ea
georgii <goqwertys@gmail.com>
2024-09-15 18:16:40 +0400

    views done

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'home.html')
 
 def contacts(request):
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'contacts.html')
