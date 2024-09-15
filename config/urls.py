430e7e6cf5250f4cfda4864ee1a32e680f0a16f8
georgii <goqwertys@gmail.com>
2024-09-15 18:15:43 +0400

    routing done

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),  # Включаем маршруты из приложения catalog
+    path('', include('catalog.urls', namespace='catalog')),
 ]
