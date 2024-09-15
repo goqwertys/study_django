8b6535b05e032a3e1f5f22401f9f4a1b7268475a
georgii <goqwertys@gmail.com>
2024-09-15 18:53:46 +0400

    successful sending

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,4 +7,9 @@ def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        return HttpResponse(f'Thanks, {name}! Message received.')
     return render(request, 'contacts.html')
