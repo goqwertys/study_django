8a3b73544fd7f2758404f1503e2ca07684aa4247
georgii <goqwertys@gmail.com>
2024-09-22 07:15:05 +0400

    product_detail added / add_product added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
+from tkinter.font import names
+
 from django.urls import path
 from catalog.apps import CatalogConfig
 
-from catalog.views import home, contacts, product_list
+from catalog.views import home, contacts, product_list, product_detail, add_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product_list/', product_list, name='products')
+    path('product_list/', product_list, name='products'),
+    path('products/<int:pk>', product_detail, name='product_detail'),
+    path('add_product/', add_product, name='add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from catalog.models import Product
+from .forms import ProductForm
 
 
 # Create your views here.
@@ -13,7 +14,12 @@ def product_list(request):
     }
     return render(request, 'product_list.html', context)
 
-
+def product_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'product_detail.html', context)
 
 def home(request):
     latest_products = Product.objects.order_by('-created_at')[:5]
@@ -26,3 +32,14 @@ def contacts(request):
         message = request.POST.get('message')
         return HttpResponse(f'Thanks, {name}! Message received.')
     return render(request, 'contacts.html')
+
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:products')  # Перенаправление на страницу со списком товаров
+    else:
+        form = ProductForm()
+    return render(request, 'add_product.html', {'form': form})
