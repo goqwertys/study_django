2ef6d574d696948002dd566899d177ad5020f9ab
georgii <goqwertys@gmail.com>
2024-10-22 03:14:14 +0400

    login/logout works

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-from django.conf.global_settings import AUTH_USER_MODEL
+from django.conf.global_settings import AUTH_USER_MODEL, LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 from dotenv import load_dotenv
 import os
 from pathlib import Path
@@ -152,3 +152,5 @@ EMAIL_HOST_PASSWORD = os.getenv('SMTP_PASSWORD')
 EMAIL_USE_TLS = True
 
 AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/catalog/'
+LOGOUT_REDIRECT_URL = '/catalog/'
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,16 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from skyblog.urls import app_name, urlpatterns
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='login.html'),)
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path(
+        'logout/', LogoutView.as_view(
+            # http_method_names=["post", "get", "options"],
+            next_page='/catalog/'),
+         name='logout'
+    ),
 ]
